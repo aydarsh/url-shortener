4af29936687800b0a4b168ce7420744f87bab66f
Aydar Sharafiev <aydarsh@gmail.com>
2022-08-17 18:28:15 +0300

    add Url domain. closes #2

--- src/main/java/com/rostertwo/urlshortener/domain/Url.java
+++ src/main/java/com/rostertwo/urlshortener/domain/Url.java
@@ -1,6 +1,18 @@
 package com.rostertwo.urlshortener.domain;
 
+import lombok.Data;
+
+import javax.persistence.*;
 import java.io.Serializable;
 
+@Entity
+@Table
+@Data
 public class Url implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String shortUrl;
+    private String longUrl;
 }
